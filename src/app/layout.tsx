5e277ec6a7093502b31ef4293e71e28d65c3d0d7
kwesi ebo <weareitv98@gmail.com>
2025-07-28 10:51:40 +0000

    optimize the app to load faster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,19 @@
 import type { Metadata } from 'next';
+import { PT_Sans, Playfair_Display } from 'next/font/google';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
+import { cn } from '@/lib/utils';
+
+const ptSans = PT_Sans({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-pt-sans',
+});
+
+const playfairDisplay = Playfair_Display({
+  subsets: ['latin'],
+  variable: '--font-playfair-display',
+});
 
 export const metadata: Metadata = {
   title: 'GoGhana Planner',
@@ -14,13 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
-      </head>
-      <body className="font-body antialiased">
+      <body className={cn("font-body antialiased", ptSans.variable, playfairDisplay.variable)}>
         {children}
         <Toaster />
       </body>
