9432b64874a5c566855381e18c1dde23a8039b32
kwesi ebo <weareitv98@gmail.com>
2025-08-01 21:04:54 +0000

    not a bad feature but we dont need that now we will consider that later

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,9 +55,6 @@ export default function RootLayout({
                     <Link href="/ideas">Trip Ideas</Link>
                 </Button>
                 <Button asChild variant="ghost">
-                    <Link href="/tours">Tours</Link>
-                </Button>
-                <Button asChild variant="ghost">
                     <Link href="/planner">Planner</Link>
                 </Button>
                 <Button asChild variant="ghost">
@@ -82,9 +79,6 @@ export default function RootLayout({
                             </SheetClose>
                              <SheetClose asChild>
                                 <Link href="/ideas">Trip Ideas</Link>
-                            </SheetClose>
-                             <SheetClose asChild>
-                                <Link href="/tours">Tours</Link>
                             </SheetClose>
                             <SheetClose asChild>
                                 <Link href="/planner">Planner</Link>
